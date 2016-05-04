88e8e101fddfe0f4e870b2ed8abf9a33d3f2f58b
Marco Fruhwirth <fruhwirth.marco@gmail.com>
2016-05-04 18:53:32 +0200

    Added missing files

--- src/learning_clojure/core.clj
+++ src/learning_clojure/core.clj
@@ -1,7 +1,21 @@
 (ns learning-clojure.core
-  (:gen-class))
+  (:gen-class)
+  (:require [learning-clojure.framework :refer :all]
+            [quil.core :as q]))
+
+(defn initialize-sketch
+  []
+  0)
+
+(defn update-sketch
+  [old-state]
+  (q/background old-state)
+  (mod (inc old-state) 255))
+
+(defn draw-sketch
+  [old-state])
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (sketch initialize-sketch draw-sketch update-sketch))
--- src/learning_clojure/framework.clj
+++ src/learning_clojure/framework.clj
@@ -1,1 +1,18 @@
-(ns learning-clojure.framework)
+(ns learning-clojure.framework
+  (:require [quil.core :as q]
+            [quil.middleware :as m]))
+
+(defn setup [initialize]
+  (q/frame-rate 60)
+  (q/background 200)
+  (initialize))
+
+(defmacro sketch
+  [initialize draw update]
+  `(q/defsketch ~'example-sketch
+                :title "Example"
+                :setup (partial setup ~initialize)
+                :draw ~draw
+                :update ~update
+                :middleware [m/fun-mode]
+                :size [600 400]))
